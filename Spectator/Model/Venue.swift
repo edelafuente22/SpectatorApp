530a8d8fe89f1f9b2eb61213e562d00e3e55aafe
edelafuente22 <ofthefountain22@gmail.com>
2018-04-25 16:58:25 -0700

    begun setup of choosing fav teams function

---
+++ Spectator/Model/Venue.swift
@@ -0,0 +1,9 @@
+//
+//  Venue.swift
+//  Spectator
+//
+//  Created by Edward de la Fuente on 4/25/18.
+//  Copyright © 2018 Claitco. All rights reserved.
+//
+
+import Foundation
---
+++ Spectator/Model/VenueList.swift
@@ -0,0 +1,9 @@
+//
+//  VenueList.swift
+//  Spectator
+//
+//  Created by Edward de la Fuente on 4/25/18.
+//  Copyright © 2018 Claitco. All rights reserved.
+//
+
+import Foundation
